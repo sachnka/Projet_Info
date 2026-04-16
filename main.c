5425f223bac9257bc3b77a1c9576bb344a19b63e
Alex <lehiralexandre4@gmail.com>
2026-04-16 10:08:54 +0200

    menu fonctionnel

---
+++ main.c
@@ -0,0 +1,42 @@
+#include "game.h"
+
+int main() {
+    allegro_init();
+    install_keyboard();
+    set_color_depth(32);
+    set_gfx_mode(GFX_AUTODETECT_WINDOWED, GAME_W, GAME_H, 0, 0);
+
+    BITMAP *buffer = create_bitmap(GAME_W, GAME_H);
+
+    load_menu_assets();
+
+    int choix = menu_choix(buffer);
+    switch (choix) {
+
+        case 1:
+            allegro_message("Nouvelle partie !");
+            // TODO : lancer ton jeu ici
+            break;
+
+        case 2:
+            allegro_message("Charger une partie !");
+            // TODO : charger une sauvegarde
+            break;
+
+        case 3:
+            allegro_message("Les règles !");
+            // TODO : afficher les règles
+            break;
+
+        case 4:
+            allegro_message("Quitter !");
+            return 0;
+
+        default:
+            break;
+    }
+
+    destroy_bitmap(buffer);
+    return 0;
+}
+END_OF_MAIN();
--- menu.c
+++ menu.c
@@ -3,7 +3,7 @@
 BITMAP *background = NULL;
 
 void load_menu_assets() {
-    background = load_bitmap("image/jojo.bmp", NULL);
+    background = load_bitmap("jojo.bmp", NULL);
 
     if (!background) {
         allegro_message("Impossible de charger jojo.bmp !");
@@ -20,9 +20,33 @@ void draw_menu(BITMAP *b) {
 
     textout_centre_ex(b, font, "SUPER BULLES",
                       GAME_W/2, 200, makecol(255,255,255), -1);
-
     textout_centre_ex(b, font, "1 - Nouvelle partie",
                       GAME_W/2, 260, makecol(200,200,200), -1);
+    textout_centre_ex(b, font, "2 - Charger une partie",
+                       GAME_W/2, 300, makecol(200,200,200), -1);
+    textout_centre_ex(b, font, "3 - Les règles",
+                   GAME_W/2, 340, makecol(200,200,200), -1);
+    textout_centre_ex(b, font, "4 - Quitter",
+               GAME_W/2, 380, makecol(200,200,200), -1);
 
     blit(b, screen, 0, 0, 0, 0, GAME_W, GAME_H);
 }
+
+int menu_choix(BITMAP *buffer) {
+    int choix = 0;
+
+    while (!key[KEY_ESC]) {
+
+        draw_menu(buffer);
+
+        if (key[KEY_1]) { choix = 1; break; }
+        if (key[KEY_2]) { choix = 2; break; }
+        if (key[KEY_3]) { choix = 3; break; }
+        if (key[KEY_4]) { choix = 4; break; }
+
+        rest(10);
+    }
+
+    return choix;
+}
+
